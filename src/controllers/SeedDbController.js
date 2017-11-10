15bc071895cb8501ad38efadad2ebb58e3bdfa60
SDouyer <33057764+SDouyer@users.noreply.github.com>
2017-11-10 16:18:06 +0100

    Getting rid of BookingModel related errors

--- src/controllers/SeedDbController.js
+++ src/controllers/SeedDbController.js
@@ -3,13 +3,13 @@ import Errors from "../helpers/Errors";
 
 // Récupération du model
 import ShowModel from "../models/ShowModel";
-import BookingModel from "../models/BookingModel";
+//import BookingModel from "../models/BookingModel";
 
 export default {
   seedDb: (req, res) => {
     return Promise.all([
       ShowModel.deleteShows(),
-      BookingModel.deleteBookings(),
+      //BookingModel.deleteBookings(),
     ])
     .then((data) => {
       return Promise.all([
